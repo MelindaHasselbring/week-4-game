6d523198c55b30d99525ce50f201d77069849a54
Melinda Hasselbring <melinda.hasselbring@gmail.com>
2017-05-01 19:50:15 -0700

    submit1

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+ 
+// var crystal = {
+//       "color:" "white";
+//       "font-family:" times, "sans-serif";
+//       "font-size:" "30px";
+//       "border-style:" "groove";
+//       "border-radius:" "2px";
+//       "border:" "1px solid #A44531";
+//       "padding:" "10px"; 
+//       "margin:" "10px 5px 5px 20px";
+//       "color:" "white";
+//       "background:" "#A44531";
+// }
+
+
+$("#crystals").addClass("game");
