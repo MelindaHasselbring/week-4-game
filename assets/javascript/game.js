845c5ae3311a9589033cb180948df4cadc74cc9e
Melinda Hasselbring <melinda.hasselbring@gmail.com>
2017-05-01 22:15:18 -0700

    submit1

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,4 +13,90 @@
 // }
 
 
-$("#crystals").addClass("game");
+// $("#crystals").addClass("game");
+
+
+// random # between 19 & 120  
+ var goal = Math.floor((Math.random()*120) +19);   
+ var score = 0;
+ var wins = 0;
+ var losses = 0;
+
+
+// random # between 1 & 12 for each crystal-icon 
+ var diamondValue = Math.floor((Math.random()*12) +1);
+ var spadeValue  = Math.floor((Math.random()*12) +1);
+ var heartValue  = Math.floor((Math.random()*12) +1);
+ var clubsValue  = Math.floor((Math.random()*12) +1);
+
+ //set the diamond-icon as a button and clicable.
+
+ $("#diamond-icon").on("click", function() {
+ 	alert ("diamond");
+    score += diamondValue;
+
+    alert("Your total score is: " + score);
+
+    if (score === goal) {
+      alert("You win!");
+      win ++;
+    }
+
+    else if (score>= goal) {
+      alert("You lose!!");
+      losses++;
+        }
+  });
+
+$("#spade-icon").on("click", function() {
+ 	alert ("spade");
+    score += spadeValue;
+
+    alert("Your total score is: " + score);
+
+    if (score === goal) {
+      alert("You win!");
+      win ++;
+      reset;
+    }
+
+    else if (score>= goal) {
+      alert("You lose!!");
+      losses++;
+      reset;
+        }
+  });
+
+$("#heart-icon").on("click", function() {
+
+    score += heartValue;
+	alert ("heart");
+    alert("Your total score is: " + score);
+
+    if (score === goal) {
+      alert("You win!");
+      win ++;
+    }
+
+    else if (score>= goal) {
+      alert("You lose!!");
+      losses++;
+        }
+  });
+
+$("#clubs-icon").on("click", function() {
+ 	alert ("clubs");
+    score += clubsValue;
+
+    alert("Your total score is: " + score);
+
+    if (score === goal) {
+      alert("You win!");
+      win ++;
+    }
+
+    else if (score>= goal) {
+      alert("You lose!!");
+      losses++;
+        }
+  });
